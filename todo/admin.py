3e49a175a2460c38cd7c806f7f50603ab1cb7c3d
ogs13 <nb.x100e@gmail.com>
2022-01-06 18:56:48 +0200

    tests

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'date',
+        'done'
+    ) 
+    list_display_links = (
+        'id',
+        'title'
+    )
+    search_fields = (
+        'id',
+        'title',
+        'description'
+    )
+    #list_editable = ()
+    list_filter = (
+        'date',
+        'done'
+    )
+
+admin.site.register(Todo, TodoAdmin)
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoTest(TestCase):
+    """ Test module for Puppy model """
+    def setUp(self):
+        Todo.objects.create(
+            title='first todo', 
+            description='first todo description'
+        )
+        Todo.objects.create(
+            title='second todo', 
+            description='second todo description',
+            done=True
+        )
+    
+    def test_todo(self):
+        first_todo = Todo.objects.get(title='first todo')
+        self.assertEqual(first_todo.description, 'first todo description')
+        self.assertEqual(first_todo.done, False)
+        
+        second_todo = Todo.objects.get(title='second todo')
+        self.assertEqual(second_todo.description, "second todo description")
+        self.assertEqual(second_todo.done,True)
